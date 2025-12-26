bcb06db074cbbdbfc3c56d46f9f4bce7685b99c5
Ancientmemer <ancientmemer12@gmail.com>
2025-12-26 17:21:23 +0530

    Update filters.py

--- database/filters.py
+++ database/filters.py
@@ -1,16 +1,26 @@
-from .mongodb import db
+from database.mongodb import db
 
-filters_col = db["filters"]
+
+COLLECTION = db.filters
+
 
 async def add_filter(chat_id, keyword, data):
-    await filters_col.update_one(
+    data["chat_id"] = chat_id
+    data["keyword"] = keyword
+
+    await COLLECTION.update_one(
         {"chat_id": chat_id, "keyword": keyword},
         {"$set": data},
         upsert=True
     )
 
+
 async def remove_filter(chat_id, keyword):
-    await filters_col.delete_one({"chat_id": chat_id, "keyword": keyword})
+    await COLLECTION.delete_one(
+        {"chat_id": chat_id, "keyword": keyword}
+    )
+
 
 async def get_filters(chat_id):
-    return filters_col.find({"chat_id": chat_id})
+    cursor = COLLECTION.find({"chat_id": chat_id})
+    return await cursor.to_list(length=100)
