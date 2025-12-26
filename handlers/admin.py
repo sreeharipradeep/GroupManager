640cd9903d0d5e4858e3a26605253e31096cd5e8
Ancientmemer <ancientmemer12@gmail.com>
2025-12-26 14:15:52 +0530

    Update admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,10 +3,32 @@ from utils.typing import typing
 
 def register_admin(app):
 
-    @app.on_message(filters.command("pin") & filters.group)
-    async def pin_msg(client, message):
+    @app.on_message(filters.command("ban") & filters.group)
+    async def ban(client, message):
         if not message.reply_to_message:
-            return await message.reply("Reply to a message to pin.")
+            return
         await typing(client, message.chat.id)
-        await message.reply_to_message.pin()
-        await message.reply("📌 Pinned.")
+        await message.chat.ban_member(message.reply_to_message.from_user.id)
+        await message.reply("🚫 Banned.")
+
+    @app.on_message(filters.command("mute") & filters.group)
+    async def mute(client, message):
+        if not message.reply_to_message:
+            return
+        await typing(client, message.chat.id)
+        await message.chat.restrict_member(
+            message.reply_to_message.from_user.id,
+            permissions={}
+        )
+        await message.reply("🔇 Muted.")
+
+    @app.on_message(filters.command("purge") & filters.group)
+    async def purge(_, message):
+        if not message.reply_to_message:
+            return
+        ids = range(message.reply_to_message.id, message.id)
+        for i in ids:
+            try:
+                await message.chat.delete_messages(i)
+            except:
+                pass
